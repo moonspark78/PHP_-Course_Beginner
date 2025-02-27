aa5b4113c671aa8be05ad8ce8b49702fcf945151
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-02-27 03:06:02 +0100

    Part 2: Variable  :art:

--- variables.php
+++ variables.php
@@ -2,4 +2,25 @@
     $name = "Course";
     $myFirstName= "toto";
     $my_last_name = "tata";
+
+    /* 
+Noms de Variables Invalides ❌
+Commence par un chiffre
+$123age → Erreur : Parse error: syntax error
+
+Caractères spéciaux (sauf _)
+$nom-utilisateur (tiret), $email@site (@), $prix#euro (#)
+
+Espaces dans le nom
+$nom utilisateur → Erreur
+
+Syntaxe incorrecte
+$variable! (!), $%pourcentage (%)
+
+Points dans le nom
+$nom.variable → Interprété comme un objet ou une concaténation
+
+Mots-clés réservés (dans certains contextes)
+$function, $class, $echo → À éviter (peut causer des conflits).
+    */
 ?>
