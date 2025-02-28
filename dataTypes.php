59d5d7b69084934ebca04c45b53776cd9916d104
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-02-28 02:21:27 +0100

    working on the  dataTypes file  :art:

--- dataTypes.php
+++ dataTypes.php
@@ -2,5 +2,15 @@
     echo "DATA TYPES ";
 
     $name = "Souli";
-    $sentence= "I love PHP";
+    $sentence= "I love PHP"; // String
+
+
+
+    $age = 25; // Integer
+    $ageMinus = -25; // Integer
+
+    $price = 25.99; // Float
+
+    $isTrue = true; // Boolean
+    $isFalse = false; // Boolean
 ?>
