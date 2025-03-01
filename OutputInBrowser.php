79880ad8039256e0bc883ae64a87fec97a51d3d0
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-01 02:33:00 +0100

    Output in Browser  :art:

---
+++ OutputInBrowser.php
@@ -0,0 +1,23 @@
+<?php
+    echo "Output in Browser";
+
+    $name = "Souli";
+    $age = 25;
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+
+    var_dump($age);
+    echo "<br>";
+
+
+    print_r($name);
+    echo "<br>";
+    
+    echo($name);
+    echo "<br>";
+    echo " lolo"
+?>
