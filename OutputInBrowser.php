a2a01d23958208ff0984090056b2ce05dd42138d
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-01 02:56:53 +0100

    Output in Browser with print  :art:

--- OutputInBrowser.php
+++ OutputInBrowser.php
@@ -22,4 +22,10 @@
     echo "----------------";
     echo "Hello World!"; // Affiche "Hello World!"
     echo 42; // Affiche "42"
+    echo "<br>";
+    echo "----------------";
+    //Utilisation : Similaire à echo, mais ne prend qu'un seul argument.
+    print "Bonjour"; // Affiche "Bonjour"
+    $resultat = print "Hello"; // $resultat = 1
+
 ?>
