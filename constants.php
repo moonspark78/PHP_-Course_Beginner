8095af878c553d14206261a61eeb98a2a69dc414
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-02 03:36:58 +0100

    Part 6 : constants   :art:

---
+++ constants.php
@@ -0,0 +1,12 @@
+<?php
+        echo "Constants";
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+
+        // define(CONSTANT_NAME, VALUE);
+        define("PI", 3.14);
+        var_dump(PI); // float(3.14)
+
+
+?>
--- indirectReference.php
+++ indirectReference.php
@@ -21,4 +21,11 @@
         Ainsi, $$user = "David" devient $Souli = "David" (une nouvelle variable $Souli est créée).
         $user n'est pas modifiée, donc echo $user affiche toujours "Souli".
     */
+
+    /* 
+            Différence entre $user et $$user :
+                $user : Variable classique contenant une valeur directe (ex: "Souli").
+                $$user : Variable variable qui utilise la valeur de $user comme nom de variable.
+                Si $user = "Souli", alors $$user équivaut à $Souli.
+    */
 ?>
