5fd592776cb9d02e49d56069ccf4669a8411cb83
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-02 03:04:39 +0100

    Part 5: Indirect Reference  :art:

--- indirectReference.php
+++ indirectReference.php
@@ -7,5 +7,18 @@
 
     $user = "Souli";
     $user = "David";
-    echo $user;
+    echo $user; // Affiche "David"
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+    $user = "Souli";
+    $$user = "David"; // Équivaut à $Souli = "David"
+    echo $user; // Affiche "Souli"
+
+    /* 
+        Ainsi, $$user = "David" devient $Souli = "David" (une nouvelle variable $Souli est créée).
+        $user n'est pas modifiée, donc echo $user affiche toujours "Souli".
+    */
 ?>
