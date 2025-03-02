f4385019ea5d8172f1ef95afe3171c28b26ca628
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-02 03:42:11 +0100

    Part 6 : done with  constants   :art:

--- constants.php
+++ constants.php
@@ -6,7 +6,10 @@
 
         // define(CONSTANT_NAME, VALUE);
         define("PI", 3.14);
+        define("TEST", "soso la mouche");
         var_dump(PI); // float(3.14)
+        echo "<br>";
+        var_dump(TEST); // string(soso la mouche)
 
 
 ?>
