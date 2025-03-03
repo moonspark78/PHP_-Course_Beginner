d2007728d0c1bc94658f54b73ad4b96ebe872b85
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-03 15:57:42 +0100

    part : Arithmetic Operator  continu  :art:

--- arithmeticOperator.php
+++ arithmeticOperator.php
@@ -9,5 +9,12 @@
     $num1 = 10;
     $num2 = 5;
     echo "Addition:   $num1 + $num2  = " . ($num1 + $num2);
+    echo "<br>";
+
+    echo $num1 - $num2;
+    echo "<br>";
+    echo $num1 * $num2;
+    echo "<br>";
+    echo $num1 / $num2;
 
 ?>
