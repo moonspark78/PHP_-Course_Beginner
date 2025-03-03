22c51e0bb719f94efbedd8893e76d9652f5ea6b1
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-03 15:47:26 +0100

    part : Arithmetic Operator  :art:

---
+++ arithmeticOperator.php
@@ -0,0 +1,13 @@
+<?php
+    echo "Arithmetic Operator";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+    //Arithmetic Operator
+    $num1 = 10;
+    $num2 = 5;
+    echo "Addition:   $num1 + $num2  = " . ($num1 + $num2);
+
+?>
