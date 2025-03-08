4831fafc9bb7fc6272f0a9edcfc52b3667b9e32e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-08 15:09:40 +0100

    🔥 part 9: Comparison Operator  :art:

---
+++ comparisonOperators.php
@@ -0,0 +1,12 @@
+<?php
+    echo "Comparison Operator";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+    $num1 = 10;
+    $num2 = 5;
+
+    var_dump($num1 == $num2); //bool(false)
+    echo "<br>";
+?>
