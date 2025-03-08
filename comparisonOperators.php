1f1d7b72def802730641b0933e99aa631c1cc390
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-08 15:13:19 +0100

    🔥 part 9: Comparison Operator  ===  :art:

--- comparisonOperators.php
+++ comparisonOperators.php
@@ -6,7 +6,11 @@
 
     $num1 = 10;
     $num2 = 5;
+    $num3="10";
 
     var_dump($num1 == $num2); //bool(false)
+    echo "<br>";
+
+    var_dump($num1 === $num3); //bool(false) car le type de $num1 est int et le type de $num3 est string
     echo "<br>";
 ?>
